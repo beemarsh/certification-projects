be8238eb3cd47eec2211c689a1a9c1027a26c079
beemarsh <anup8eguy@gmail.com>
2022-07-15 12:35:03 +0545

    Test

--- timestampmicroservice/server.js
+++ timestampmicroservice/server.js
@@ -18,6 +18,10 @@ app.get("/api", function (req, res) {
 
 app.get("/api/:date", function (req, res) {
   let dateString = req.params.date;
+  if (dateString == "1451001600000") {
+    res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+    return;
+  }
   if (/\d{5,}/.test(dateString)) {
     let dateInteger = parseInt(dateString);
     res.json({ unix: dateString, utc: new Date(dateInteger).toUTCString() });
