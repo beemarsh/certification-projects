f41d824da8f7f832bca7f44058afc1f89459715f
beemarsh <anup8eguy@gmail.com>
2022-07-15 12:10:26 +0545

    Pass test

--- timestampmicroservice/server.js
+++ timestampmicroservice/server.js
@@ -1,37 +1,55 @@
+var http = require("http");
+var path = require("path");
 var express = require("express");
-var app = express();
+var strftime = require("strftime");
+var router = express();
+var server = http.createServer(router);
+
 require("dotenv").config();
 
-var cors = require("cors");
-app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+router.use(express.static("public"));
 
-app.use(express.static("public"));
-
-app.get("/", function (req, res) {
+router.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api", function (req, res) {
-  res.json({ unix: Date.now(), utc: Date() });
+// route on GET with a parameter we call :date
+router.get("/api/:date", function (req, res) {
+  // creating a date object
+  var date = new Date();
+  // if the given parameter is a number (timestamp)
+  if (/^\d*$/.test(req.params.date)) {
+    date.setTime(req.params.date);
+  }
+  // else we just create a new date parsing the string given
+  else {
+    date = new Date(req.params.date);
+  }
+
+  // giving headers for JSON
+  res.set({ "Content-Type": "application/json" });
+  // if the date is invalid
+  if (!date.getTime())
+    res.send(JSON.stringify({ error: "Invalid date given" }));
+  // else, we send the object with two members (unix and natural)
+  else
+    res.send(
+      JSON.stringify({
+        unix: date.getTime(),
+        natural: strftime("%B %d, %Y", date),
+      })
+    );
 });
 
-app.get("/api/:date", function (req, res) {
-  let dateString = req.params.date;
-  if (/\d{5,}/.test(dateString)) {
-    let dateInteger = parseInt(dateString);
-    res.json({ unix: dateString, utc: new Date(dateInteger).toUTCString() });
+// listening to port and processing
+server.listen(
+  process.env.PORT || 3000,
+  process.env.IP || "0.0.0.0",
+  function () {
+    var addr = server.address();
+    console.log(
+      "Timestamp microservice listening at",
+      addr.address + ":" + addr.port
+    );
   }
-  let dateObj = new Date(dateString);
-  dateObj.setMinutes(date.getMinutes()-3);
-  if (dateObj.toString() === "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    res.json({ unix: dateObj.valueOf(), utc: dateObj.toUTCString() });
-  }
-});
-
-var listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + process.env.PORT);
-  console.log(`URL : http://localhost:${process.env.PORT}`)
-});
+);
